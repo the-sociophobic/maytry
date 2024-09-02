894a40bd46f33e6d699d31278796b745fedcec9c
the-sociophobic <torikowashi@gmail.com>
2024-09-02 20:15:33 +0300

    add routes-Loader on contentful preload fix extendedFilter close

--- src/components/ProtectedRoutes/index.tsx
+++ src/components/ProtectedRoutes/index.tsx
@@ -3,15 +3,47 @@ import React, { useEffect, useMemo } from 'react'
 import { RouterProvider, createHashRouter } from 'react-router-dom'
 
 import useStore from '../../hooks/useStore'
-// import Loader from '../Loader'
+import Loader from '../Loader'
 import routes, { RouteType } from './routes'
-// import useUser from '../../hooks/useUser'
-// import Login from '../../pages/Login'
 import Layout from '../Layout'
-import useContentful from '../../hooks/useContentful'
+import useContentful, { ContentfulType } from '../../hooks/useContentful'
 import Item from '../../pages/Item'
 import PageTemplate from '../PageTemplate'
 import Redirect from '../Redirect'
+
+
+export type ProtectedRoutesProps = {
+  contentRef: React.RefObject<HTMLDivElement>
+}
+
+const ProtectedRoutes: React.FC<ProtectedRoutesProps> = () => {
+  const { user } = useStore()
+  const { data: contentful } = useContentful()
+
+  useEffect(() => useStore.setState({ user }), [user])
+  
+  const router = useMemo(
+    () => createHashRouter(
+      mapRoutes([
+        ...(user ?
+          routes.slice(2)
+          :
+          routes.filter(route => route.to !== '/account')
+        ),
+        ...(!contentful ? [] : mapContentfulRoutes(contentful))
+      ])
+    )
+    , [contentful, user]
+  )
+
+  if (!contentful)
+    return <Loader />
+
+  return <RouterProvider router={router} />
+}
+
+
+export default ProtectedRoutes
 
 
 const mapRoutes = (
@@ -28,47 +60,15 @@ const mapRoutes = (
     errorElement: <Redirect to='/' />
   }))
 
-
-export type ProtectedRoutesProps = {
-  contentRef: React.RefObject<HTMLDivElement>
-}
-
-const ProtectedRoutes: React.FC<ProtectedRoutesProps> = () => {
-  // const { data: user, isLoading: loadingUser } = useUser()
-  const { user } = useStore()
-  const { data: contentful } = useContentful()
-
-  const router = useMemo(
-    () => createHashRouter(
-      // mapRoutes(user ? routes : routes.slice(-2))
-      mapRoutes([
-        ...(user ?
-          routes.slice(2)
-          :
-          routes.filter(route => route.to !== '/account')
-        ),
-        ...(contentful?.items.map(item => ({
-          to: item.link,
-          title: item.name,
-          Comp: <Item {...item} />
-        })) || []),
-        ...(contentful?.pages.map(page => ({
-          to: page.link.link,
-          title: page.link.title,
-          Comp: <PageTemplate {...page} />
-        })) || [])
-      ])
-    )
-    , [contentful, user]
-  )
-
-  useEffect(() => useStore.setState({ user }), [user])
-  
-  // if (loadingUser)
-  //   return <Loader />
-
-  return <RouterProvider router={router} />
-}
-
-
-export default ProtectedRoutes
+const mapContentfulRoutes = (contentful: ContentfulType) => [
+  ...(contentful.items.map(item => ({
+    to: item.link,
+    title: item.name,
+    Comp: <Item {...item} />
+  })) || []),
+  ...(contentful.pages.map(page => ({
+    to: page.link.link,
+    title: page.link.title,
+    Comp: <PageTemplate {...page} />
+  })) || [])
+]
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef } from 'react'
+import { FC, useEffect, useRef } from 'react'
 
 import useContentful from '../hooks/useContentful'
 import ItemCard from '../components/ItemCard'
@@ -15,18 +15,50 @@ export type MainProps = {}
 
 const Main: FC<MainProps> = ({ }) => {
   const { data: contentful } = useContentful()
+
+  const { showStartBanner } = useStore()
+
+  const { showSearch } = useStore()
   const { searchString } = useStore()
-  const { filterBy } = useStore()
+  const { setSearchString } = useStore()
+
   const { showFilter } = useStore()
+  const { filterBy } = useStore()
+  const { setFilterBy } = useStore()
+
   const { showExtendedFilter } = useStore()
   const { sortOrder } = useStore()
   const { mainPageView } = useStore()
-  const { showStartBanner } = useStore()
+
   const { priceFrom } = useStore()
+  const { setPriceFrom } = useStore()
   const { priceTo } = useStore()
+  const { setPriceTo } = useStore()
   const { selectedColorIds } = useStore()
+  const { setSelectedColorIds } = useStore()
   const { selectedSizesIds } = useStore()
+  const { setSelectedSizesIds } = useStore()
+
   const catalogRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!showSearch)
+      setSearchString('')
+  }, [showSearch])
+
+  useEffect(() => {
+    if (!showFilter)
+      setFilterBy([])
+  }, [showFilter])
+
+  useEffect(() => {
+    if (!showExtendedFilter) {
+      setPriceFrom(undefined)
+      setPriceTo(undefined)
+      setSelectedColorIds([])
+      setSelectedSizesIds([])
+    }
+  }, [showExtendedFilter])
 
   return (
     <ScrollToConsumer>
@@ -57,33 +89,33 @@ const Main: FC<MainProps> = ({ }) => {
           >
             {contentful?.items
               .filter(item =>
-                searchString.length === 0 ||
-                  item.name.toLocaleLowerCase().includes(searchString.toLocaleLowerCase())
+                !showSearch || searchString.length === 0 ||
+                item.name.toLocaleLowerCase().includes(searchString.toLocaleLowerCase())
               )
               .filter(item =>
-                (filterBy.length === 0 || !showFilter) ||
-                  item.categories?.map(itemCategory => itemCategory.name)
-                    .some(itemCategoryName => filterBy.includes(itemCategoryName))
+                (!showFilter || filterBy.length === 0) ||
+                item.categories?.map(itemCategory => itemCategory.name)
+                  .some(itemCategoryName => filterBy.includes(itemCategoryName))
               )
               .filter(item => {
-                if (priceFrom === undefined)
+                if (!showExtendedFilter || priceFrom === undefined)
                   return true
                 return getCurrentPrice(item) >= priceFrom
               })
               .filter(item => {
-                if (priceTo === undefined)
+                if (!showExtendedFilter || priceTo === undefined)
                   return true
                 return getCurrentPrice(item) <= priceTo
               })
               .filter(item => {
-                if (selectedColorIds.length === 0)
+                if (!showExtendedFilter || selectedColorIds.length === 0)
                   return true
                 return item.color_price_size
                   ?.find(c_p_s =>
                     selectedColorIds.includes(c_p_s.color?.id || ''))
               })
               .filter(item => {
-                if (selectedSizesIds.length === 0)
+                if (!showExtendedFilter || selectedSizesIds.length === 0)
                   return true
                 return item.color_price_size
                   ?.find(c_p_s =>
