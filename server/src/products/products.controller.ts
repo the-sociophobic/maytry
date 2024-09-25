0e2e9473a23caccee4768d83d31e173b94b0751b
the-sociophobic <torikowashi@gmail.com>
2024-09-25 09:00:19 +0300

    add 1C items parser

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
-import { ProductsService } from './products.service';
 import axios from 'axios';
+
+import { ProductsService } from './products.service';
+import { ColorPriceSizeType, getProductsResponce, ParsedProductType, ProductType } from './products.type';
 
 
 @Controller('products')
@@ -8,9 +10,9 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
-  async getProducts(): Promise<any> {
+  async getProducts(): Promise<ParsedProductType[]> {
     try {
-      const res: any = await axios.get(
+      const { items: items_from_1C } = (await axios.get<getProductsResponce>(
         'https://1c.yoot.pro:4443/trade3/hs/catalog/products',
         {
           auth: {
@@ -18,12 +20,42 @@ export class ProductsController {
             password: '8gW6lXBU4Xxig2'
           }
         }
-      )
-  
-      return res.data
+      )).data
+      const items_from_1C_parsed_map = new Map<string, ParsedProductType>()
+
+      items_from_1C.forEach(item => {
+        const itemName = getItemName(item)
+        let item_in_map = items_from_1C_parsed_map.get(itemName)
+        const color_price_size: ColorPriceSizeType = {
+          color: item.color,
+          price: item.price,
+          size: item.size,
+          max_available: item.count,
+        }
+
+        if (!item_in_map)
+          item_in_map = {
+            name: itemName,
+            color_price_size: []
+          }
+
+        items_from_1C_parsed_map.set(itemName, {
+          ...item_in_map,
+          color_price_size: [...item_in_map.color_price_size, color_price_size]
+        })
+      })
+
+      return [...items_from_1C_parsed_map].map(([_name, value]) => value)
+
     } catch (err) {
       console.log(err);
       throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
+}
+
+const getItemName = (item: ProductType) => {
+  const number_of_spaces = (item.color.match(/ /g) || []).length + ((item.size + '').match(/ /g) || []).length + 2
+
+  return item.name.split(' ').slice(0, -number_of_spaces).join(' ')
 }
