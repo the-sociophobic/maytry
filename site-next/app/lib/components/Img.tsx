aa426ff184921ef0db462c6729717752716bceb3
the-sociophobic <torikowashi@gmail.com>
2025-07-02 22:05:30 +0300

    upd Img, combined.json

--- site-next/app/lib/components/Img.tsx
+++ site-next/app/lib/components/Img.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import Image from 'next/image'
-
-import React, { FC, useState } from 'react'
+import React, { FC, useRef, useState } from 'react'
 
 import { ContentfulFile } from '../types/contentful.type'
 
@@ -23,42 +21,50 @@ type ImgProps = {
 
 const Img: FC<ImgProps> = (props) => {
   const [portrait, setPortrait] = useState<boolean | undefined>(undefined)
-  const imgRef: any = React.createRef()
-  const containerRef: any = React.createRef()
+  const imgRef = useRef<HTMLImageElement>(null)
+  const containerRef = useRef<HTMLDivElement>(null)
 
-  const setOrientation = () =>
-    setPortrait(containerRef?.current?.offsetWidth / containerRef?.current?.offsetHeight >
-        imgRef?.current?.width / imgRef?.current?.height
-    )
+  const setOrientation = () => {
+    const container = containerRef.current
+    const img = imgRef.current
 
-    const src = `${props.src || props?.file?.file?.url || ''}${props.urlParams || ''}`
+    if (!container || !img)
+      return
 
-    return src.length === 0 ? <></> : (
-      <div
-        ref={containerRef}
+    const containerRatio = container.offsetWidth / container.offsetHeight
+    const imgRatio = img.width / img.height
+
+    setPortrait(containerRatio > imgRatio)
+  }
+
+  const src = `${props.src || props?.file?.file?.url || ''}${props.urlParams || ''}`
+
+  return src.length === 0 ? <></> : (
+    <div
+      ref={containerRef}
+      className={`
+        Img
+        ${props.className}
+        ${typeof portrait === "undefined" && "Img--hidden"}
+      `}
+      style={props.style}
+      onClick={props.onClick}
+    >
+      {/* <Image */}
+      <img
+        ref={imgRef}
+        alt={props.alt || props?.file?.file?.fileName || 'image'}
+        src={!src.includes('http') ? src.replace('//', 'https://') : src}
         className={`
-          Img
-          ${props.className}
-          ${typeof portrait === "undefined" && "Img--hidden"}
+          Img__img
+          Img__img--${props.crop && (portrait ? "portrait" : "landscape")}
         `}
-        style={props.style}
-        onClick={props.onClick}
-      >
-        {/* <Image */}
-        <img
-          ref={imgRef}
-          alt={props.alt || props?.file?.file?.fileName || 'image'}
-          src={!src.includes('http') ? src.replace('//', 'https://') : src}
-          className={`
-            Img__img
-            Img__img--${props.crop && (portrait ? "portrait" : "landscape")}
-          `}
-          onLoad={setOrientation}
-          // width={props.width}
-          // height={props.height}
-        />
-      </div>
-    )
+        onLoad={setOrientation}
+      // width={props.width}
+      // height={props.height}
+      />
+    </div>
+  )
 }
 
 
